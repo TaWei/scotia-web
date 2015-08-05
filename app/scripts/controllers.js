cc92c5df4605828b64c6e36f21a3070bfb69eddf
Wei Sing Ta <james.mma@gmail.com>
2015-08-04 22:48:03 -0400

    Added basic grid functionality and angular grid

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -52,6 +52,36 @@ angular.module('starter.controllers', [])
   ];
 }])
 
-.controller('RankingsCtrl', ['$scope', '$stateParams', function($scope,$stateParams) {
+.controller('RankingsCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+  var columnDefs = [
+      {headerName: "Ticker", field: "ticker"},
+      {headerName: "Closing", field: "closingPrice"},
+      {headerName: "MACD Weekly", field: "macdWeekly"},
+      {headerName: "Signal Weekly", field: "signalWeekly"},
+      {headerName: "MACD Weekly Change", field: "macdWeeklyChange"},
+      {headerName: "Signal Weekly Change", field: "signalWeeklyChange"},
+      {headerName: "MACD Monthly", field: "macdMonthly"},
+      {headerName: "Signal Monthly", field: "signalMonthly"},
+      {headerName: "MACD Monthly Change", field: "macdMonthlyChange"},
+      {headerName: "Signal Monthly Change", field: "signalMonthlyChange"}
+  ];
 
+  var rowData = [
+      {ticker: "TSX: AAB", closingPrice: 4.35, macdWeekly: 1422, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 },
+      {ticker: "TSX: MAG", closingPrice: 12.35, macdWeekly: 124, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 },
+      {ticker: "TSX: MAK", closingPrice: 44.35, macdWeekly: 213, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 },
+      {ticker: "TSX: MBA", closingPrice: 7.35, macdWeekly: 123, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 },
+      {ticker: "TSX: MBB.UN", closingPrice: 123.56, macdWeekly: 123, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 },
+      {ticker: "TSX: SBI", closingPrice: 532.55, macdWeekly: 123, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 },
+      {ticker: "TSX: SCL", closingPrice: 56.25, macdWeekly: 123, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 },
+      {ticker: "TSX: SCU", closingPrice: 78.74, macdWeekly: 123, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 },
+      {ticker: "TSX: USB", closingPrice: 37.35, macdWeekly: 123, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 },
+      {ticker: "TSX: WDO", closingPrice: 3783.35, macdWeekly: 123, signalWeekly: 3.0, macdWeeklyChange: 10.0, signalWeeklyChange: 5.3, macdMonthly: 25000 , signalMonthly: 2333, macdMonthlyChange: 2.0, signalMonthlyChange: 12.44 }
+  ];
+
+  $scope.gridOptions = {
+      columnDefs: columnDefs,
+      rowData: rowData,
+      pinnedColumnCount: 2
+  };
 }]);
